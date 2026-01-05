bd63915db48bbd0bdce3de2d94644d60c00a5377
Ryz-24 <ruba.z@live.com>
2026-01-06 00:25:15 +0300

    Optimize sorting process in push_swap
    Refactor sorting logic to avoid unnecessary sorting when stack is already sorted.

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -12,32 +12,35 @@
 
 #include "push_swap.h"
 
+static void	sort_stack(t_stack *a, t_stack *b)
+{
+	if (a->size == 2)
+		sort_2(a);
+	else if (a->size == 3)
+		sort_3(a);
+	else if (a->size == 4)
+		sort_4(a, b);
+	else if (a->size == 5)
+		sort_5(a, b);
+	else
+		chunk_sort(a, b);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack	a;
 	t_stack	b;
 
-	init_stack(&a);
-	init_stack(&b);
 	if (ac < 2)
 		return (0);
+	init_stack(&a);
+	init_stack(&b);
 	parse_args(&a, ac, av);
-	if (is_sorted(&a))
+	if (!is_sorted(&a))
 	{
-		free_stack(&a);
-		return (0);
+		index_stack(&a);
+		sort_stack(&a, &b);
 	}
-	index_stack(&a);
-	if (a.size == 2)
-		sort_2(&a);
-	else if (a.size == 3)
-		sort_3(&a);
-	else if (a.size == 4)
-		sort_4(&a, &b);
-	else if (a.size == 5)
-		sort_5(&a, &b);
-	else
-		chunk_sort(&a, &b);
 	free_stack(&a);
 	free_stack(&b);
 }
