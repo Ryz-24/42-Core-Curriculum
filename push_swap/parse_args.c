bd53a3036a2311a83f122b5d493c29ae58baa781
Ryz-24 <ruba.z@live.com>
2026-01-05 23:53:11 +0300

    decrease argument parsing lines
    Refactor argument parsing and error handling in parse_args.c.

--- push_swap/parse_args.c
+++ push_swap/parse_args.c
@@ -12,72 +12,54 @@
 
 #include "push_swap.h"
 
-void	free_split(char **arr)
+static void	handle_error(t_stack *a, char **parts)
 {
-	int	i;
-
-	if (!arr)
-		return ;
-	i = 0;
-	while (arr[i])
-		free(arr[i++]);
-	free(arr);
+	free_split(parts);
+	free_stack(a);
+	error_exit();
 }
 
-static int	check_duplicate(t_stack *a, int value)
+static void	parse_value(t_stack *a, char *str, char **parts)
 {
-	t_node	*nod;
+	long	v;
 
-	nod = a->top;
-	while (nod)
-	{
-		if (nod->value == value)
-			return (1);
-		nod = nod->next;
-	}
-	return (0);
+	if (!ft_isnumber(str))
+		handle_error(a, parts);
+	v = ft_atoi(str);
+	if (v < INT_MIN || v > INT_MAX)
+		handle_error(a, parts);
+	if (check_duplicate(a, (int)v))
+		handle_error(a, parts);
+	push_bottom(a, new_node((int)v));
 }
 
-void	free_stack(t_stack *s)
+static void	parse_parts(t_stack *a, char **parts)
 {
-	t_node	*tmp;
+	int	j;
 
-	while (s->top)
+	j = 0;
+	while (parts[j])
 	{
-		tmp = s->top;
-		s->top = s->top->next;
-		free(tmp);
+		parse_value(a, parts[j], parts);
+		j++;
 	}
-	s->bottom = NULL;
-	s->size = 0;
 }
 
 void	parse_args(t_stack *a, int ac, char **av)
 {
-	int			i;
-	int			j;
-	char		**parts;
-	long		v;
+	int		i;
+	char	**parts;
 
 	i = 1;
 	while (i < ac)
 	{
 		parts = ft_split(av[i], ' ');
 		if (!parts)
-			error_exit();
-		j = 0;
-		while (parts[j])
 		{
-			if (!ft_isnumber(parts[j]))
-				(free_split(parts), free_stack(a), error_exit());
-			v = ft_atoi(parts[j]);
-			if (v < INT_MIN || v > INT_MAX)
-				(free_split(parts), free_stack(a), error_exit());
-			if (check_duplicate(a, (int)v))
-				(free_split(parts), free_stack(a), error_exit());
-			push_bottom(a, new_node((int)v));
-			j++;
+			free_stack(a);
+			error_exit();
 		}
+		parse_parts(a, parts);
 		free_split(parts);
 		i++;
 	}
