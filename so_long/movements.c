945c331c3a74e4922c3664871847ed66cfeab5da
Ryz-24 <ruba.z@live.com>
2026-01-24 03:56:31 +0300

    Refactor player movement logic in movements.c
    Refactor move_player function to reduce redundancy and improve readability.

--- so_long/movements.c
+++ so_long/movements.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   movements.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rzaatreh <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/17 15:26:35 by rzaatreh          #+#    #+#             */
-/*   Updated: 2026/01/17 15:26:37 by rzaatreh         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "so_long.h"
 
 static void	move_player(t_game *game, int dx, int dy)
@@ -19,27 +7,37 @@ static void	move_player(t_game *game, int dx, int dy)
 
 	new_x = game->player_x + dx;
 	new_y = game->player_y + dy;
-	if (game->map[new_y][new_x] == '1')
-		return ;
+
 	if (new_x < 0 || new_y < 0
 		|| new_x >= game->width || new_y >= game->height)
 		return ;
+
+	if (game->map[new_y][new_x] == '1')
+		return ;
+
 	if (game->map[new_y][new_x] == 'C')
 		game->collectible--;
+
 	if (game->map[new_y][new_x] == 'E' && game->collectible != 0)
 		return ;
-	if (game->map[new_y][new_x] == 'E' && game->collectible == 0)
-		clean_exit(game);
-	if (game->map[game->player_y][game->player_x] != 'E')
-		game->map[game->player_y][game->player_x] = '0';
-	game->map[new_y][new_x] = 'P';
-	game->player_x = new_x;
-	game->player_y = new_y;
+
 	game->moves++;
 	ft_putnbr_fd(game->moves, 1);
 	ft_putchar_fd('\n', 1);
+
+	if (game->map[new_y][new_x] == 'E' && game->collectible == 0)
+		clean_exit(game);
+
+	if (game->map[game->player_y][game->player_x] != 'E')
+		game->map[game->player_y][game->player_x] = '0';
+
+	game->map[new_y][new_x] = 'P';
+	game->player_x = new_x;
+	game->player_y = new_y;
+
 	render_map(game);
 }
+
 
 int	handle_key(int keycode, t_game *game)
 {
