cf026e641580bae0370f0da19b4d6731ace170d8
Ryz-24 <ruba.z@live.com>
2026-01-24 03:57:57 +0300

    Refactor so_long.h for improved readability
    Updated the structure and comments in so_long.h for clarity.

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -1,25 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rzaatreh <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/17 15:27:04 by rzaatreh          #+#    #+#             */
-/*   Updated: 2026/01/17 15:27:07 by rzaatreh         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SO_LONG_H
-# define SO_LONG_H
+#define SO_LONG_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-# include <fcntl.h>
-# include "libft/libft.h"
-# include "get_next_line/get_next_line.h"
-# include "minilibx-linux/mlx.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#include "get_next_line/get_next_line.h"
+#include "minilibx-linux/mlx.h"
 
 # define KEY_ESC 65307
 
@@ -35,23 +23,31 @@
 
 typedef struct s_game
 {
-	void	*mlx;
-	void	*win;
-	char	**map;
-	int		width;
-	int		height;
-	int		player_x;
-	int		player_y;
-	int		collectible;
-	int		moves;
-	void	*img_wall;
-	void	*img_floor;
-	void	*img_player;
-	void	*img_exit;
-	void	*img_collectible;
+	void	*mlx; //pointer to the MiniLibX context
+	void	*win; //pointer to the game window
+	
+	char	**map; //the characters representing the map
+	int	width; //number of columns in the map
+	int	height; //number of the rows
+	
+	int	player_x; //player's column index
+	int	player_y; //player's row index
+	
+	int	collectible;
+	int	moves;
+
+	/* Each pointer holds an MLX image */
+	void	*img_wall; // '1'
+	void	*img_floor; // '0'
+	void	*img_player; // 'P'
+	void	*img_exit; // 'E'
+	void	*img_collectible; //'C'
+
 }	t_game;
 
-void	error_msg(char *msg);
+
+void	error_msg(t_game *game, char *msg);
+int	handle_expose(t_game *game);
 
 void	validate_map(t_game *game);
 void	check_path(t_game *game);
@@ -59,11 +55,11 @@ void	check_path(t_game *game);
 void	read_map(t_game *game, char *file);
 
 void	render_map(t_game *game);
-int		handle_key(int keycode, t_game *game);
+int	handle_key(int keycode, t_game *game);
 void	load_images(t_game *game);
 void	init_mlx(t_game *game);
 
 void	free_map(char **map);
-int		clean_exit(t_game *game);
+int	clean_exit(t_game *game);
 
 #endif
