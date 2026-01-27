eb09ba68c4c20a136f26e873797e1b54f3935cde
Ryz-24 <ruba.z@live.com>
2026-01-27 15:42:26 +0300

    Refactor signal handling in server.c

--- minitalk/server.c
+++ minitalk/server.c
@@ -1,40 +1,52 @@
-#include "libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzaatreh <rzaatreh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 14:24:55 by rzaatreh          #+#    #+#             */
+/*   Updated: 2026/01/27 15:41:23 by rzaatreh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf/ft_printf.h"
 #include <signal.h>
-#include <stdio.h>
 #include <unistd.h>
 
-#define END '\0'
-
 /*Receives the MSB first and ends with the LSB by shifting to the left*/
-void	handle_signal(int signal)
+static void	handle_signal(int sig, siginfo_t *info, void *context)
 {
-	static unsigned char	current_char;
-	static int				bit_index;
+	static unsigned char	c;
+	static int				bits;
 
-	/*
-	if (signal == SIGUSR1)
-    current_char = current_char | 1;
-	*/
-	current_char |= (signal == SIGUSR1);
-	bit_index++;
-	if (bit_index == 8)
+	(void)context;
+	c <<= 1;
+	if (sig == SIGUSR1)
+		c |= 1;
+	bits++;
+	if (bits == 8)
 	{
-		if (current_char == END)
-			ft_printf("\n");
+		if (c == '\0')
+			write(1, "\n", 1);
 		else
-			ft_printf("%c", current_char);
-		bit_index = 0;
-		current_char = 0;
+			write(1, &c, 1);
+		bits = 0;
+		c = 0;
 	}
-	else
-		current_char <<= 1;
+	kill(info->si_pid, SIGUSR1);
 }
 
 int	main(void)
 {
+	struct sigaction	sa;
+
 	ft_printf("%d\n", getpid());
-	signal(SIGUSR1, handle_signal);
-	signal(SIGUSR2, handle_signal);
+	sa.sa_sigaction = handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
