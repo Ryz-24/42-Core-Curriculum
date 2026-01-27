6ab0b441939c1fbbd33210a7174b1def6b0d2acf
Ryz-24 <ruba.z@live.com>
2026-01-27 15:48:53 +0300

    Add files via upload

---
+++ minitalk/ft_printf/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzaatreh <rzaatreh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 12:28:58 by rzaatreh          #+#    #+#             */
+/*   Updated: 2026/01/27 15:28:12 by rzaatreh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	num;
+
+	sign = 1;
+	num = 0;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		num = (num * 10) + (*nptr - '0');
+		nptr++;
+	}
+	return (num * sign);
+}
+/*
+int main()
+{
+	char	*a =  " -1234ab567";
+	printf("%d\n", ft_atoi(a));
+}*/
