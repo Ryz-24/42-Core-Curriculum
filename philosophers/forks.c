64cac067a95ae3543697536b4abfbfe2d0bb7d65
Ryz-24 <ruba.z@live.com>
2026-05-21 15:45:41 +0300

    Refactor fork locking mechanism in philosophers

--- philosophers/forks.c
+++ philosophers/forks.c
@@ -12,30 +12,16 @@
 
 #include "philo.h"
 
-static int	lock_fork_safe(pthread_mutex_t *fork, t_data *data)
-{
-	while (!is_simulation_stopped(data))
-	{
-		if (pthread_mutex_trylock(fork) == 0)
-			return (1);
-		usleep(100);
-	}
-	return (0);
-}
-
 static int	take_forks_even(t_philo *philo)
 {
 	t_data	*data;
 
 	data = philo->data;
-	if (!lock_fork_safe(&data->forks[philo->right_fork], data))
+	if (is_simulation_stopped(data))
 		return (0);
+	pthread_mutex_lock(&data->forks[philo->right_fork]);
 	print_state(philo, "has taken a fork");
-	if (!lock_fork_safe(&data->forks[philo->left_fork], data))
-	{
-		pthread_mutex_unlock(&data->forks[philo->right_fork]);
-		return (0);
-	}
+	pthread_mutex_lock(&data->forks[philo->left_fork]);
 	print_state(philo, "has taken a fork");
 	return (1);
 }
@@ -45,14 +31,11 @@ static int	take_forks_odd(t_philo *philo)
 	t_data	*data;
 
 	data = philo->data;
-	if (!lock_fork_safe(&data->forks[philo->left_fork], data))
+	if (is_simulation_stopped(data))
 		return (0);
+	pthread_mutex_lock(&data->forks[philo->left_fork]);
 	print_state(philo, "has taken a fork");
-	if (!lock_fork_safe(&data->forks[philo->right_fork], data))
-	{
-		pthread_mutex_unlock(&data->forks[philo->left_fork]);
-		return (0);
-	}
+	pthread_mutex_lock(&data->forks[philo->right_fork]);
 	print_state(philo, "has taken a fork");
 	return (1);
 }
